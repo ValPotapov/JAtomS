09eef7fba13db8f860d2fcc5cdd0ba955fdb7a34
Septdir <septdir@gmail.com>
2019-06-21 17:38:24 +0300

    Reset to dev

--- mod_jatoms_showcases/media/js/ajax.js
+++ mod_jatoms_showcases/media/js/ajax.js
@@ -1,6 +1,6 @@
 /*
  * @package    Joomla Atom-S Showcases Module
- * @version    1.1.0
+ * @version    __DEPLOY_VERSION__
  * @author     Septdir Workshop - septdir.com
  * @copyright  Copyright (c) 2018 - 2019 Septdir Workshop. All rights reserved.
  * @license    GNU/GPL license: https://www.gnu.org/copyleft/gpl.html
